525e0fd9744a48b077d89a8e12644ce18d1ad3c1
MarkVanAcker <mark.vanacker@student.kuleuven.be>
2019-11-19 22:19:40 +0100

    fixed SerialCom + added DB

--- project/project.ino
+++ project/project.ino
@@ -6,21 +6,37 @@
 #include <semphr.h>
 #include <avr/wdt.h>
 #include <avr/power.h>
+#include <EDB.h>
+#include <EEPROM.h>
+
 #define SCK     15
 #define MISO    14
 #define MOSI    16
 #define SS      8
 #define RST     4
 #define DI0     7
-#define BAND    868E6 //915E6
+#define BAND    8695E5 //915E6
 #define PABOOST true
+
+// Uncomment the line appropriate for your platform
+#define TABLE_SIZE 512 // Arduino 168 or greater
+
+// The number of demo records that should be created.  This should be less 
+// than (TABLE_SIZE - sizeof(EDB_Header)) / sizeof(LogEvent).  If it is higher, 
+// operations will return EDB_OUT_OF_RANGE for all records outside the usable range.
+#define RECORDS_TO_CREATE 10
 
 #define READ_LATEST_TEMP  1
 #define READ_ALL_TEMP     2
 #define ENABLE_LOW_OP     3
 
+
+
 int packetCounter = 0;
 int sleepTime = 0;
+
+int currentRecordNo = 0;
+float currentTemp;
 String incomingPacket;
 String inputPacket = "Ping";
 bool sending = true;
@@ -30,6 +46,28 @@ TaskHandle_t receiveBeaconTaskHandle;
 TickType_t receiveDelay;
 TaskHandle_t userCommandTaskHandle;
 // TickType_t receiveDelay;
+
+
+struct DBEntry {
+  int sleepduration;
+  float temperature;
+} 
+dBEntry;
+
+// The read and write handlers for using the EEPROM Library
+void writer(unsigned long address, byte data)
+{
+  EEPROM.write(address, data);
+}
+
+byte reader(unsigned long address)
+{
+  return EEPROM.read(address);
+}
+
+
+EDB db(&writer, &reader);
+
 void setup() 
 {
   Serial.begin(9600);                             // Set data rate in bits for serial transmission
@@ -42,6 +80,10 @@ void setup()
     while(1);
   }
 
+  db.create(0, TABLE_SIZE, sizeof(dBEntry));
+
+  writeToDB(1,2);
+  writeToDB(2,4);
   /* Setup tasks */
   /* Send Beacon Task */
   xTaskCreate(
@@ -49,7 +91,7 @@ void setup()
       "receiveBeacon",
       128,  // Stack size
       NULL, //paramaters task
-      2,    // priority
+      1,    // priority
       &receiveBeaconTaskHandle);
 
   /* User Command Task */
@@ -58,7 +100,7 @@ void setup()
       "handleUserCommands",
       128,  // Stack size
       NULL, //paramaters task
-      1,    // priority
+      2,    // priority
       &userCommandTaskHandle);
 }
 
@@ -67,28 +109,52 @@ void loop()
   
 }
 
+
+void readLatest()
+{
+    db.readRec(currentRecordNo, EDB_REC dBEntry);
+    Serial.print("Time: "); Serial.println(dBEntry.sleepduration);
+    Serial.print("Temp: "); Serial.println(dBEntry.temperature);  
+}
+
+void readAll()
+{
+  int i;
+  for( i= 0; i < currentRecordNo; i++)
+  {
+    db.readRec(i, EDB_REC dBEntry);
+    Serial.print("Time: "); Serial.println(dBEntry.sleepduration);
+    Serial.print("Temp: "); Serial.println(dBEntry.temperature);  
+  }
+  
+}
+
 void handleUserCommands(void *pvParameters)
 {
   while(1)
   {
-    int userInput = NULL;
-    if (Serial.available() != 0)userInput = Serial.parseInt();
+    int userInput;
+
+    while (Serial.available() == 0){
+      vTaskDelay(1);  // One tick delay in between reads so that other processes get the chance to run.
+    }
+    userInput = Serial.parseInt();
 
     if (userInput == READ_LATEST_TEMP)
     { 
       Serial.println("Execute - Read latest temperature entry");
-      
+      readLatest();
     }
     else if (userInput == READ_ALL_TEMP)
     {
       Serial.println("Execute - Read all temperature entries");
+      readAll();
     }
     else if (userInput == ENABLE_LOW_OP) 
     {
       Serial.println("Execute - Enable low operation mode");
     }
-
-    vTaskDelay(1);  // One tick delay in between reads for stability.
+    
   }
 }
 
@@ -101,7 +167,9 @@ void receiveBeacon(void *pvParameters)
   {
 //    receive0 = millis();
     sleepTime = receive();
-    send(sleepTime, chipTemp(chipTempRaw()));
+    currentTemp = chipTemp(chipTempRaw());
+    send(sleepTime, currentTemp);
+    writeToDB(sleepTime, currentTemp);
     packetCounter++;
     milisBefore = millis();
     receiveDelay = (sleepTime - (0.045 * sleepTime) - 150) / portTICK_PERIOD_MS;
@@ -117,11 +185,23 @@ void receiveBeacon(void *pvParameters)
 void send(int sleepTime, float temp) {
   byte* sleepTimeBytes = (byte*)&sleepTime;
   byte* tempBytes = (byte*)&temp;
-  
+
   LoRa.beginPacket();
   LoRa.write(sleepTimeBytes, sizeof(sleepTime));
   LoRa.write(tempBytes, sizeof(temp));
   LoRa.endPacket();
+}
+
+void writeToDB(int sleepTime, float temp){
+
+    if( currentRecordNo == RECORDS_TO_CREATE)
+    {
+        Serial.println("Table full exception");
+    }
+    currentRecordNo++;
+    dBEntry.sleepduration = sleepTime;
+    dBEntry.temperature = temp;
+    db.appendRec(EDB_REC dBEntry);
 }
 
 int receive()
